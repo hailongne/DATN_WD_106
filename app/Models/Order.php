b5d6498ea1fcaf1384d8c39f3f86b2b2dc420cc6
Hiuhiu10 <ngoisao00304@gmail.com>
2025-01-01 22:24:57 +0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -42,7 +42,7 @@ class Order extends Model
     }
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'order_product', 'order_id', 'product_id');
+        return $this->belongsToMany(Product::class, 'order_items', 'order_id', 'product_id');
     }
     public function items()
     {
