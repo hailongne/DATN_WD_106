68334e4a4efd838ead0825e8d3fa81e3bed5ddda
longdh <nongthanthien2004@gmail.com>
2025-01-13 02:27:14 +0700

    logo-image-binhluan

--- resources/views/admin/pages/product/detail.blade.php
+++ resources/views/admin/pages/product/detail.blade.php
@@ -58,7 +58,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    
+
     @endpush
 
 @endsection
